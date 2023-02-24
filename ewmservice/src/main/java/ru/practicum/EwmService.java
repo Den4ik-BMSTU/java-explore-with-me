98e3d6fb2ff244b516c632380dc9b1f156958a22
Denic113 <denic113@yandex.ru>
2023-02-24 21:03:19 +0300

    V1.9

--- ewmservice/src/main/java/ru/practicum/EwmService.java
+++ ewmservice/src/main/java/ru/practicum/EwmService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainService {
+public class EwmService {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainService.class, args);
+        SpringApplication.run(EwmService.class, args);
     }
     //
 }
